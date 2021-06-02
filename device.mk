e453dc55c8f61ef87750ba6bc081a01f95cdaa16
Mesquita <mesquita@aospa.co>
2021-06-02 08:33:23 -0300

    apollo: Switch overlays to RRO
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- device.mk
+++ device.mk
@@ -5,12 +5,11 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pa
-
 PRODUCT_PACKAGES += \
-    AOSPAApolloFrameworks
+    AOSPAApolloFrameworks \
+    AOSPAApolloSystemUI \
+    ApolloFrameworks \
+    ApolloSystemUI
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
