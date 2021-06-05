85e79580c195cedd617f0d06f6c8156b3c8a2f99
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-06-05 11:08:49 -0300

    apollo: Enable content based surface flinger refresh rate setting
    Co-authored-by: Ramii Ahmed <ramy@ahmedramy.com>
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- device.mk
+++ device.mk
@@ -23,6 +23,14 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# Refrash rate
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.set_idle_timer_ms=200 \
+    ro.surface_flinger.set_touch_timer_ms=200 \
+    vendor.display.idle_time=1100
+
 # Inherit from sm8250-common
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
