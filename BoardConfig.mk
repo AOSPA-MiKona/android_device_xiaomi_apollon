903c332f897e065035aa2b2afa8fa46ea399636d
Mesquita <mesquita@aospa.co>
2021-06-15 22:36:26 -0300

    apollo: Move dt2w node to common
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,9 +14,6 @@ BUILD_BROKEN_DUP_RULES := true
 # Display
 TARGET_SCREEN_DENSITY := 440
 
-# Gestures
-TARGET_TAP_TO_WAKE_EVENT_NODE  := "/sys/touchpanel/double_tap"
-
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_apollo
 TARGET_RECOVERY_DEVICE_MODULES := libinit_apollo
